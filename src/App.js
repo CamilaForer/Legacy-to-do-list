1613b397bb0e09f9d3cd5e8632762891a9c5404d
Camila Forero <amandaforero@outlook.com>
2023-01-20 11:01:24 -0500

    Boostrap test

--- src/App.js
+++ src/App.js
@@ -3,52 +3,57 @@ import todos from "./apis";
 import Form from "./components/Form";
 import Section from "./components/Section";
 import List from "./components/List";
+import Cards from "./components/Cards";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const appTitle = "To-Do App";
-
 const App = () => {
-    const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const { data } = await todos.get("/todos");
-            setTodoList(data);
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await todos.get("/todos");
+      setTodoList(data);
+    }
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    const addTodo = async (item) => {
-        const { data } = await todos.post("/todos", item);
-        setTodoList((oldList) => [...oldList, data]);
-    };
+  const addTodo = async (item) => {
+    const { data } = await todos.post("/todos", item);
+    setTodoList((oldList) => [...oldList, data]);
+  };
 
-    const removeTodo = async (id) => {
-        await todos.delete(`/todos/${id}`);
-        setTodoList((oldList) => oldList.filter((item) => item._id !== id));
-    };
+  const removeTodo = async (id) => {
+    await todos.delete(`/todos/${id}`);
+    setTodoList((oldList) => oldList.filter((item) => item._id !== id));
+  };
 
-    const editTodo = async (id, item) => {
-        await todos.put(`/todos/${id}`, item);
-    };
+  const editTodo = async (id, item) => {
+    await todos.put(`/todos/${id}`, item);
+  };
 
-    return (
-        <div className="ui container center aligned">
-            <Section>
-                <h1>{appTitle}</h1>
-            </Section>
-            <Section>
-                <Form addTodo={addTodo} />
-            </Section>
-            <Section>
-                <List
-                    editTodoListProp={editTodo}
-                    removeTodoListProp={removeTodo}
-                    list={todoList}
-                />
-            </Section>
-        </div>
-    );
+  return (
+    <div className="ui container center aligned">
+      <Section>
+        <h1>{appTitle}</h1>
+      </Section>
+      <Section>
+        <Form addTodo={addTodo} />
+      </Section>
+      <Section>
+        <List
+          editTodoListProp={editTodo}
+          removeTodoListProp={removeTodo}
+          list={todoList}
+        />
+      </Section>
+      <Section>
+        <Cards/>
+      </Section>
+
+    </div>
+  );
 };
 
 export default App;
