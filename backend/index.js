7cdf0290dd21783a1c2d6c7ff239bb4084b86f18
DruckDeveloper <druckdev@gmail.com>
2023-01-20 16:48:15 -0500

    project conected with mongodb in mongo atlas

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true, use
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost/todolist", connectionOptions)
+mongoose.connect("mongodb+srv://appuser:Todo0147Project@todolist.xyepi7r.mongodb.net/?retryWrites=true&w=majority", connectionOptions)
     .then(() => console.log("Connected successfully"))
     .catch((err) => console.error(err));
 
