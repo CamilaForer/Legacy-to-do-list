4959f2431aba0616d6bb7760b7e0aa9d56e1d82c
Camila Forero <amandaforero@outlook.com>
2023-01-23 18:56:44 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 const app = express();
 
 const todoRoutes = require("./routes/todoRoutes");
