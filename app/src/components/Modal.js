9029e18916b6a54813022a52f3973c7d79506305
yeray004 <yeye20041@gmail.com>
2023-01-22 22:30:51 -0500

    Changes on Form component and general styles of ToDo component

--- app/src/components/Modal.js
+++ app/src/components/Modal.js
@@ -4,8 +4,8 @@ const Modal = (_) => {
     return (
         <>
             {/* Button trigger modal */}
-            <button type="button"class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Launch demo modal 
+            <button type="button"class="btn-modal" data-toggle="modal" data-target="#exampleModal">
+                📂
                 {/* BsFillCollectionFill */}
                 {/* AiFillFolderOpen */}
             </button>
