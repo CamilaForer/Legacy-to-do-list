bc44c05a424e3ef91877d231d95c2f259d7e36cf
Camila Forero <amandaforero@outlook.com>
2023-01-23 19:43:24 -0500

    Update index.js

--- app/src/apis/index.js
+++ app/src/apis/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.BACKEND_ENDPOINT || "http://localhost:3030"
+    baseURL: process.env.BACKEND_ENDPOINT || "https://legacy-to-do-list-c00q8s2fu-camilaforer.vercel.app/"
 });
